0f5e68d919a8d081acb6f8f12f11243fefd8c11b
Francisco de Borja Cabeza Rozas <borja.cabeza@cabodigital.es>
2022-03-29 16:50:15 +0200

    Incorporación del proyecto ConsoleApp1

---
+++ Sopra.Labs.ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sopra.Labs.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
