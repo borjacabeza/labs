a87aaf35b301a8bb0d44699cb4d810052a7689a8
Francisco de Borja Cabeza Rozas <borja.cabeza@cabodigital.es>
2022-03-29 17:00:53 +0200

    Modificación saludo clase Program

--- Sopra.Labs.ConsoleApp1/Program.cs
+++ Sopra.Labs.ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace Sopra.Labs.ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo !!!");
         }
     }
 }
